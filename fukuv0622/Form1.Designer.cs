d77b64cc2663779f115eec1c1874567e67bfebd1
Nguyen Quoc Bao <130636520+quocbao1902@users.noreply.github.com>
2023-06-22 13:46:59 +0900

    fukuv0622

--- fukuv0622/Form1.Designer.cs
+++ fukuv0622/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(54, 9);
+            label1.MaximumSize = new Size(100, 25);
+            label1.MinimumSize = new Size(500, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(500, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(12, 77);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 45);
+            button1.TabIndex = 1;
+            button1.Text = "Avg";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
     }
 }
--- fukuv0622/Form1.cs
+++ fukuv0622/Form1.cs
@@ -2,9 +2,20 @@ namespace fukuv0622
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int[] data = new int[10];
+            for (int i = 0; i < data.Length; i++)
+                data[i] = rand.Next(101);
+            label1.Text = data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6] + ", " + data[7] + ", " + data[8] + ", " + data[9];
+            MessageBox.Show(((data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9]) / 10).ToString());
         }
     }
 }
